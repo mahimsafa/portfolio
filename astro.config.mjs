514bd86323f2bada371e99f44a9a9df2c9fba2c3
Mahim Safa <mahim.safa@fieldnation.com>
2026-03-31 02:58:08 +0600

    fix: address code review issues from Task 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +16,5 @@ export default defineConfig({
       theme: "nord",
     },
   },
-  integrations: [tailwind(), solidJs(), sitemap()],
+  integrations: [tailwind(), solidJs(), sitemap(), icon()],
 });
